093d9da3ca862fa204d52b842ee81b3ae178d282
LouisG <39853204+arkiaconsulting@users.noreply.github.com>
2024-10-30 14:29:49 +0100

    Extract more metadata from uploaded photos

--- src/backend/Fotos.WebApp.Tests/Adapters/ExifMetadataTests.cs
+++ src/backend/Fotos.WebApp.Tests/Adapters/ExifMetadataTests.cs
@@ -42,4 +42,48 @@ public sealed class ExifMetadataTests : IClassFixture<FotoIntegrationContext>
 
         metadata.DateTaken.Should().Be(default(DateTime?));
     }
+
+    [Theory(DisplayName = "Extracting EXIF metadata should pass")]
+    [InlineData("Adapters/test-file-4.jpg")]
+    internal async Task Test04(string photoPath)
+    {
+        await using var stream = File.OpenRead(photoPath);
+
+        var metadata = await _context.ExtractExifMetadata(stream, MediaTypeNames.Image.Jpeg);
+
+        metadata.DateTaken.Should().Be(new DateTime(2022, 08, 09, 14, 41, 26, DateTimeKind.Utc));
+        metadata.CameraBrand.Should().Be("NIKON CORPORATION");
+        metadata.CameraModel.Should().Be("NIKON D80");
+        metadata.Iso.Should().Be(320);
+        metadata.Width.Should().Be(3872);
+        metadata.Height.Should().Be(2592);
+        metadata.Aperture.Should().Be(8);
+        metadata.FocalLength.Should().Be(135);
+        metadata.IsFlash.Should().BeFalse();
+        metadata.ExposureTime.Should().Be("1/250 sec");
+        metadata.Lens.Should().Be("18-135mm f/3.5-5.6");
+        metadata.Quality.Should().Be("Fine");
+    }
+
+    [Theory(DisplayName = "Extracting EXIF metadata should pass")]
+    [InlineData("Adapters/test-file-5.jpg")]
+    internal async Task Test05(string photoPath)
+    {
+        await using var stream = File.OpenRead(photoPath);
+
+        var metadata = await _context.ExtractExifMetadata(stream, MediaTypeNames.Image.Jpeg);
+
+        metadata.IsFlash.Should().BeTrue();
+    }
+
+    [Theory(DisplayName = "Extracting EXIF metadata should pass")]
+    [InlineData("Adapters/test-file-iphone.jpeg")]
+    internal async Task Test06(string photoPath)
+    {
+        await using var stream = File.OpenRead(photoPath);
+
+        var metadata = await _context.ExtractExifMetadata(stream, MediaTypeNames.Image.Jpeg);
+
+        metadata.IsFlash.Should().BeFalse();
+    }
 }
